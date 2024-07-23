b0d90b867010f98aa7abc1b32f78db9678aff2ad
plss12 <pepoluis712@gmail.com>
2024-07-23 18:37:56 +0200

    Añadido visualización y uso manual de pygame, con fase de pruebas y arreglo de fallos

--- Lunar-Rover.py
+++ Lunar-Rover.py
@@ -96,8 +96,13 @@ class Rover:
             elif new_pos == 2:
                 self.reward -= 8
             # Recompensa negativa por chocar con otro agente
+            # Además terminamos el proceso ya que no puede haber
+            # dos agentes en una misma posición
             elif new_pos in self.env.rovers_mines_goals.keys():
                 self.reward -=10
+                obs = self.get_observation()
+                info = {}
+                return obs, self.reward, self.done, info
             # Recompensa positiva si ha llegado por primera vez a la mina
             elif new_pos == self.mine_id and self.mined == False: 
                 self.reward += 100
@@ -108,14 +113,25 @@ class Rover:
                 self.done = True
 
         # Movemos al agente a la nueva posición y en la posición que estaba 
-        # colocamos lo que había antes de que llegase el agente
-        self.env.grid[x, y] = self.env.initial_grid[x, y]
+        # colocamos lo que había en la copia inicial del mapa
+
+        # Si inicialmente en esa posición había un agente
+        # en este caso si se coloca un espacio vacio en el mapa
+        if (self.env.initial_grid[x, y] in self.env.rovers_mines_goals.keys()):
+            self.env.grid[x, y] = LunarObjects.FLOOR.value        
+
+        else:
+            self.env.grid[x, y] = self.env.initial_grid[x, y]
+
         self.env.grid[new_x, new_y] = self.agent_id
         self.position = (new_x, new_y)
         self.reward -= 1  # Penalización por movimiento
 
         obs = self.get_observation()
         info = {}
+
+        self.env.render()
+
         return obs, self.reward, self.done, info
 
     def get_observation(self):
@@ -157,14 +173,14 @@ class Rover:
             return valid_movements
 
 
+# Registrar el entorno
 gym.register(
     id='lunar-rover-v0',
-    entry_point='Rover:LunarEnv',
-)
-
+    entry_point='Lunar-Rover:LunarEnv',)
+    
 class LunarEnv(gym.Env):
 
-    metadata = {"render_modes": ["human"], "render_fps": 1}
+    metadata = {"render_modes": ["human"], "render_fps": 100}
 
     def __init__(self, n_agents, grid_size, vision_range,  render_mode=None):
         self.n_agents = n_agents
@@ -186,33 +202,35 @@ class LunarEnv(gym.Env):
         #                                         shape=(vision_range * 2 + 1, vision_range * 2 + 1), dtype=np.int32) 
                                 # for _ in range(n_agents)])
 
-        self.reset()
+        if self.render_mode == 'human':
+            pygame.init()
+            self.screen = pygame.display.set_mode((self.grid_size * 50, self.grid_size * 50))
+            pygame.display.set_caption("Lunar Rover Simulation")
+            self.clock = pygame.time.Clock()
 
     def reset(self, seed=None, options=None):
 
         # Inicializamos el mapa y creamos los agentes y sus objetivos
         self.grid = np.zeros((self.grid_size, self.grid_size), dtype=int)
         self.rovers_mines_goals = RoversObjects(self.n_agents).get_agents_mines_goals()
-    
-        # Colocamos obstáculos, agentes y objetivos en el mapa
+
+        # Colocamos obstáculos y creamos y colocamos agentes y objetivos en el mapa
         self._place_obstacles()
         self._place_rovers_mines_goals()
 
         self.initial_grid = np.copy(self.grid)
 
-        self.rovers = []
-        for (rover_id, (mine_id, goal_id)) in self.rovers_mines_goals.items():
-            start_pos = self._get_empty_position()
-            mine_pos = self._get_empty_position()
-            goal_pos = self._get_empty_position()
-            rover = Rover(self, rover_id, mine_id, goal_id, start_pos, mine_pos, goal_pos, self.vision_range)
-            self.rovers.append(rover)
-
-        if self.render_mode=='human':
-            self.render()
+        # Definir colores para los rovers, minas y metas
+        self.agent_colors = {}
+        self.mine_colors = {}
+        self.goal_colors = {}
+        self._assign_colors()
 
         obs = self.grid
         info = {}
+
+        if self.render_mode=='human':
+            self.render()
 
         return obs, info 
     
@@ -227,6 +245,7 @@ class LunarEnv(gym.Env):
             self.grid[pos] = LunarObjects.BIG_OBSTACLE.value
     
     def _place_rovers_mines_goals(self):
+        self.rovers = []
         for rover_id, (mine_id, goal_id) in self.rovers_mines_goals.items():
             rover_pos = self._get_empty_position()
             self.grid[rover_pos] = rover_id
@@ -236,6 +255,9 @@ class LunarEnv(gym.Env):
 
             goal_pos = self._get_empty_position()
             self.grid[goal_pos] = goal_id
+
+            rover = Rover(self, rover_id, mine_id, goal_id, rover_pos, mine_pos, goal_pos, self.vision_range)
+            self.rovers.append(rover)
     
     def _get_empty_position(self):
         while True:
@@ -247,62 +269,197 @@ class LunarEnv(gym.Env):
         return sum([rover.reward for rover in self.rovers])
         
     def _get_done(self):
-        return all(rover.done for rover in self.rovers)    
+        return all(rover.done for rover in self.rovers)   
+
+    def _assign_colors(self):
+        # Generar colores dinámicamente para los rovers, minas y metas
+        for agent_id, (mine_id, goal_id) in self.rovers_mines_goals.items():
+            color = (agent_id * 50 % 255, agent_id * 80 % 255, agent_id * 110 % 255)
+            self.agent_colors[agent_id] = color
+            self.mine_colors[mine_id] = color
+            self.goal_colors[goal_id] = color
     
     # Método para centralizar movimientos de los Rovers
     def step(self, actions):
 
         for rover, action in zip(self.rovers, actions):
             if rover.done:
-                print(f"Rover {rover.agent_id} ya terminado con recompensa {rover.reward}")
+                pass
+                # print(f"Rover {rover.agent_id} ya terminado con recompensa {rover.reward}")
             else:
                 if action in rover.get_movements():
                     rover.step(action)
-                    print(f"Rover {rover.agent_id} realiza movimiento {action} con recompensa {rover.reward}")
+                    # print(f"Rover {rover.agent_id} realiza movimiento {action} con recompensa {rover.reward}")
                 else:
-                    print(f"Rover {rover.agent_id} no realiza el movimiento por ser invalido")
+                    pass
+                    # print(f"Rover {rover.agent_id} no realiza el movimiento por ser invalido")
         obs = self.grid
         reward = self._get_reward()
         self.total_reward = reward
         done = self._get_done()
         info = {}
 
+        if self.render_mode == 'human':
+            self.render()
+
         return obs, reward, done, False, info
 
     def render(self, mode='human'):
-        # Código para renderizar el entorno con pygame
-        pass
+        if self.render_mode is None:
+            return
+        
+        # Manejo de eventos de Pygame
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        # Definir colores
+        floor = (200, 200, 200)
+        small_object = (0, 255, 0)
+        big_object = (255, 0, 0)
+
+        # Limpiar la pantalla
+        self.screen.fill((0, 0, 0))
+
+        font = pygame.font.Font(None, 36)
+
+        for x in range(self.grid_size):
+            for y in range(self.grid_size):
+                value = self.grid[x, y]
+                text = ""
+
+                if value == LunarObjects.FLOOR.value:
+                    color = floor
+                elif value == LunarObjects.SMALL_OBSTACLE.value:
+                    color = small_object
+                elif value == LunarObjects.BIG_OBSTACLE.value:
+                    color = big_object
+                
+                elif value in self.agent_colors.keys():
+                    color = self.agent_colors[value]
+                    text = "A"
+                elif value in self.mine_colors.keys():
+                    color = self.mine_colors[value]
+                    text = "M"
+                elif value in self.goal_colors.keys():
+                    color = self.goal_colors[value]
+                    text = "G"
+
+                pygame.draw.rect(self.screen, color, pygame.Rect(y * 50, x * 50, 50, 50))
+
+                if text:
+                    text_surface = font.render(text, True, (0, 0, 0))  # Texto en color negro
+                    text_rect = text_surface.get_rect(center=(y * 50 + 25, x * 50 + 25))  # Centrar el texto en el rectángulo
+                    self.screen.blit(text_surface, text_rect)
+
+        # Actualizar la pantalla
+        pygame.display.flip()
+
+        # Control de la tasa de actualización
+        self.clock.tick(self.metadata['render_fps'])
+
 
     def close(self):
-        # Clean up resources (optional)
-        pass
+        if self.render_mode == 'human':
+            pygame.display.quit()
+            pygame.quit()
 
-def prueba_env(n_agents):
+def prueba_manual(n_agents):   
+
+    # Iniciamos un nuevo entorno
     env = gym.make('lunar-rover-v0', render_mode='human', n_agents=n_agents, grid_size=10, vision_range=2)
+    env.reset()
 
-    # Iniciamos el entorno
+    # Establecer la lista de agentes completados
+    dones = [False] * n_agents
+    current_agent = 0
+
+    while not all(dones):
+        # Capturar eventos de teclado
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            elif event.type == pygame.KEYDOWN:
+                # Asignar acción basada en la tecla presionada
+                if event.key == pygame.K_UP:
+                    user_action = RoverAction.UP.value
+                elif event.key == pygame.K_DOWN:
+                    user_action = RoverAction.DOWN.value
+                elif event.key == pygame.K_LEFT:
+                    user_action = RoverAction.LEFT.value
+                elif event.key == pygame.K_RIGHT:
+                    user_action = RoverAction.RIGHT.value
+                elif event.key == pygame.K_SPACE:
+                    user_action = RoverAction.WAIT.value
+                else:
+                    user_action = None
+
+                if user_action is not None:
+                    rover = env.unwrapped.rovers[current_agent]
+
+                    # Si el rover a terminado buscamos uno que no haya terminado
+                    if rover.done:
+                        while rover.done:
+                            current_agent = (current_agent + 1) % n_agents
+                            rover = env.unwrapped.rovers[current_agent]
+
+                    # Comprobamos si la acción es válida en el agente
+                    if user_action in rover.get_movements():
+                        obs, reward, done, info = rover.step(user_action)
+                        # print(f"Rover {current_agent} realiza movimiento {user_action} con recompensa {reward}")
+                        dones[current_agent] = done
+                        current_agent = (current_agent + 1) % n_agents
+
+    print("Simulación completada")
+    env.close()
+
+def prueba_conjunta(n_agents):
+    
+    # Iniciamos un nuevo entorno
+    env = gym.make('lunar-rover-v0', render_mode='human', n_agents=n_agents, grid_size=10, vision_range=2)
+    env.reset()
+    done = False
+
+    # Acciones realizadas conjuntamente por el env
+    while not done:
+        actions = env.action_space.sample()
+        obs, reward, done, truncated, info = env.step(actions)
+        # print(f"Finalizada fase de ejecución con {reward} de recompensa")
+    
+    print("Simulación completada")
+    env.close()
+
+def prueba_individual(n_agents):
+    # Iniciamos un nuevo entorno
+    env = gym.make('lunar-rover-v0', render_mode='human', n_agents=n_agents, grid_size=10, vision_range=2)
     env.reset()
 
     # Acciones realizadas independientemente por cada Rover
     dones = [False]*n_agents
-    while False in dones:
+    while not all(dones):
         for i, rover in enumerate(env.unwrapped.rovers):
             if rover.done == False:
                 action = random.choice(rover.get_movements())
                 obs, reward, done, info = rover.step(action)
-                print(f"Rover {i} realiza movimiento {action} con recompensa {reward}")
+                # print(f"Rover {i} realiza movimiento {action} con recompensa {reward}")
             else:
                 dones[i] = True
-                print(f"Rover {i} ya terminado con recompensa {rover.reward}")
+                # print(f"Rover {i} ya terminado con recompensa {rover.reward}")
+
+    print("Simulación completada")
+    env.close()
+
+
+
+def main():
     
-    # Iniciamos el entorno
-    env.reset()
+    # prueba_manual(1)
+    # prueba_individual(1)
+    # prueba_conjunta(1)
 
-    # Acciones realizadas conjuntamente por el env
-    done = False
-    while done is False:
-        actions = env.action_space.sample()
-        obs, reward, done, truncated, info = env.step(actions)
-        print(f"Finalizada fase de ejecución con {reward} de recompensa")
+    env = gym.make('lunar-rover-v0', render_mode='human', n_agents=2, grid_size=10, vision_range=2)
 
-prueba_env(5)
+if __name__ == "__main__":
+    main()
